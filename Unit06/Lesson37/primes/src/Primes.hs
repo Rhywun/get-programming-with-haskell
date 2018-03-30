4b714cdbafec9a0dd1532b0b308a93457e700c60
Patrick Huffer <rhywun@rhywun.com>
2018-03-30 12:44:12 -0400

    End of Lesson37

--- Unit06/Lesson37/primes/src/Primes.hs
+++ Unit06/Lesson37/primes/src/Primes.hs
@@ -11,6 +11,22 @@ sieve (nextPrime:rest) = nextPrime : sieve noFactors
 
 isPrime :: Int -> Maybe Bool
 isPrime n
-  | n < 0 = Nothing
+  | n < 2 = Nothing
   | n >= length primes = Nothing
   | otherwise = Just (n `elem` primes)
+
+unsafePrimeFactors :: Int -> [Int] -> [Int]
+unsafePrimeFactors 0 [] = []
+unsafePrimeFactors n [] = []
+unsafePrimeFactors n (next:primes) =
+  if n `mod` next == 0
+    then next : unsafePrimeFactors (n `div` next) (next : primes)
+    else unsafePrimeFactors n primes
+
+primeFactors :: Int -> Maybe [Int]
+primeFactors n
+  | n < 2 = Nothing
+  | n >= length primes = Nothing
+  | otherwise = Just (unsafePrimeFactors n primesLessThanN)
+  where
+    primesLessThanN = filter (<= n) primes
--- Unit06/Lesson37/primes/test/Spec.hs
+++ Unit06/Lesson37/primes/test/Spec.hs
@@ -3,21 +3,36 @@ import           Primes
 import           Test.QuickCheck
 
 prop_validPrimesOnly val =
-  if val < 0 || val >= length primes
+  if val < 2 || val >= length primes
     then isNothing result
     else isJust result
   where
     result = isPrime val
 
-prop_primesArePrime val =
-  (result /= Just True) || null divisors
+prop_primesArePrime val = (result /= Just True) || null divisors
   where
     result = isPrime val
     divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
 
+prop_nonPrimesAreComposite val = (result /= Just False) || not (null divisors)
+  where
+    result = isPrime val
+    divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+
+prop_factorsMakeOriginal val =
+  isNothing result || (product (fromJust result) == val)
+  where
+    result = primeFactors val
+
+prop_allFactorsPrime val = isNothing result || all (== Just True) resultsPrime
+  where
+    result = primeFactors val
+    resultsPrime = map isPrime (fromJust result)
+
 main :: IO ()
 main = do
   quickCheck prop_validPrimesOnly
-  quickCheck prop_primesArePrime
-
--- cont. p. 475
+  quickCheckWith stdArgs {maxSuccess = 1000} prop_primesArePrime
+  quickCheckWith stdArgs {maxSuccess = 1000} prop_nonPrimesAreComposite
+  quickCheck prop_factorsMakeOriginal
+  quickCheck prop_allFactorsPrime

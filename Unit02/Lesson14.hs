8d639a6262ebfbd1ed5b1c22502bde934faca3e7
Patrick Huffer <rhywun@rhywun.com>
2018-07-04 13:50:48 -0400

    End of Lesson15

--- Unit02/Lesson14.hs
+++ Unit02/Lesson14.hs
@@ -2,82 +2,119 @@ module Lesson14 where
 
 import           Data.List
 
-data NewEngland
-  = CT
-  | MA
-  | ME
-  | NH
-  | RI
-  | VT
+-- Consider this
+
+data NewEngland = CT | MA | ME | NH | RI | VT
 
 instance Show NewEngland where
-  show x =
-    case x of
-      CT -> "Connecticut"
-      MA -> "Massachussetts"
-      ME -> "Maine"
-      NH -> "New Hampshire"
-      RI -> "Rhode Island"
-      VT -> "Vermont"
+  show x = case x of CT -> "Connecticut"
+                     MA -> "Massachussetts"
+                     ME -> "Maine"
+                     NH -> "New Hampshire"
+                     RI -> "Rhode Island"
+                     VT -> "Vermont"
 
 --
+-- A type in need of classes
 --
--- A type in need of classes;
--- Implementing Show;
--- Default implementation and minimum complete definitions;
--- Implementing Ord;
+
+data SixSidedDie' = S1' | S2' | S3' | S4' | S5' | S6'
+
+--
+-- Implementing Show
+--
+
+instance Show SixSidedDie' where
+  show S1' = "one"
+  show S2' = "two"
+  show S3' = "three"
+  show S4' = "four"
+  show S5' = "five"
+  show S6' = "six"
+
+--
+-- Type classes and polymorphism
+--
+
+poly1 :: Int
+poly1 = read "10" :: Int -- 10
+
+poly2 :: Double
+poly2 = read "10" :: Double -- 10.0
+
+--
+-- Default implementation and minimum complete definitions
+--
+
+-- Notice we don't have to implement (/=):
+instance Eq SixSidedDie' where
+  (==) S6' S6' = True
+  (==) S5' S5' = True
+  (==) S4' S4' = True
+  (==) S3' S3' = True
+  (==) S2' S2' = True
+  (==) S1' S1' = True
+  (==) _ _   = False
+
+-- Of course, this is the same as deriving (Eq)
+
+-- QC2
+-- RealFrac's minimal complete definition is `properFraction`.
+
+--
+-- Implementing Ord
+--
+
+instance Ord SixSidedDie' where
+  compare S6' S6' = EQ
+  compare S6' _   = GT
+  compare _ S6'   = LT
+  compare S5' S5' = EQ
+  compare S5' _   = GT
+  compare _ S5'   = LT
+  compare _ _   = undefined -- added to let it compile; in reality
+                            -- this needs many more cases
+
+--
 -- To derive or not to derive?
 --
---
-data SixSidedDie
-  = S1
-  | S2
-  | S3
-  | S4
-  | S5
-  | S6
-  deriving (Eq, Ord, Enum)
 
-instance Show SixSidedDie where
-  show S1 = "one"
-  show S2 = "two"
-  show S3 = "three"
-  show S4 = "four"
-  show S5 = "five"
-  show S6 = "six"
+-- At the end of the day, we're better off using `deriving` whenever possible
 
---
--- Q1402
--- RealFrac's minimal complete definition is `properFraction`.
---
--- NOTE: the space after `S1` is required here:
--- [S1 ..] == [one,two,three,four,five,six]
---
+data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6 deriving (Show, Eq, Ord, Enum)
+
+-- Now we can take advantage of Enum too:
+{-
+[S1 ..] -- [S1,S2,S3,S4,S5,S6]
+-}
+
 --
 -- Type classes for more-complex types
 --
---
--- was "data" but hlint suggests "newtype"
-newtype Name =
-  Name (String, String)
-  deriving (Show, Eq)
 
+-- see text for discussion of `newtype`
+newtype Name = Name (String, String) deriving (Show, Eq)
+
+names :: [Name]
 names =
-  [ Name ("Emil", "Cioran")
-  , Name ("Eugene", "Thacker")
-  , Name ("Friedrich", "Nietzsche")
-  ] :: [Name]
+  [Name ("Emil", "Cioran"), Name ("Eugene", "Thacker"), Name ("Friedrich", "Nietzsche")]
 
--- Sort by last name
+-- Sort by last name:
 instance Ord Name where
   compare (Name (f1, l1)) (Name (f2, l2)) = compare (l1, f1) (l2, f2)
 
+{-
+sort names -->
+[Name ("Emil","Cioran"),Name ("Friedrich","Nietzsche"),Name ("Eugene","Thacker")]
+-}
+
 --
--- Q1401
-data Boo
-  = Tru
-  | Fls
-  deriving (Enum)
+-- Summary
+--
+
+-- Q1
+
+data Boo = Tru | Fls deriving (Enum)
 
 instance Eq Boo where
   x == y = fromEnum x == fromEnum y
@@ -85,10 +122,8 @@ instance Eq Boo where
 instance Ord Boo where
   compare x y = compare (fromEnum x) (fromEnum y)
 
-
-
---
--- Q1402
+-- Q2
+{-
 class Integral a => Die a where
   sides :: a
   roll :: a -> a
@@ -101,3 +136,4 @@ instance Die FiveSidedDie where
 -- PASS
 -- I'm trying to come up with a class Die that holds the number of sides so I can
 -- have subclasses that don't look like S1 | S2 | etc. etc. but I can't figure it out.
+-}
--- Unit02/Lesson15.hs
+++ Unit02/Lesson15.hs
@@ -1,49 +1,48 @@
 module Lesson15 where
 
 --
---
 --  Ciphers for beginners: ROT13
 --
---
-data FourLetterAlphabet
-  = L1
-  | L2
-  | L3
-  | L4
-  deriving (Eq, Show, Enum, Bounded)
+
+data FourLetterAlphabet = L1 | L2 | L3 | L4 deriving (Eq, Show, Enum, Bounded)
 
 -- Rotate an enum `enum` halfway around an alphabet of size `size`
--- E.g. rotN 4 L4 == L2
-rotN :: (Bounded a, Enum a) => Int -> a -> a
-rotN size enum = toEnum rotation -- E.g. L2
-  where
-    half = size `div` 2 -- E.g. 2
-    offset = fromEnum enum + half -- E.g. 3 + 2 == 5
-    rotation = offset `mod` size -- E.g. 5 `mod` 4 == 1
+-- (N.B. - I don't see a need for Bounded here)
+{-
+rotN 4 L4 -- L2
+-}
+rotN :: (Enum a) => Int -> a -> a
+rotN size enum = toEnum rotation   -- E.g. L2
+ where
+  half     = size `div` 2          -- E.g. 2
+  offset   = fromEnum enum + half  -- E.g. 3 + 2 == 5
+  rotation = offset `mod` size     -- E.g. 5 `mod` 4 == 1
 
 -- Char-specific rotN
 rotChar :: Char -> Char
-rotChar = rotN size
-  where
-    size = 1 + fromEnum (maxBound :: Char)
+rotChar = rotN size where size = 1 + fromEnum (maxBound :: Char)
 
-message = [L1, L3, L4, L1, L1, L2] :: [FourLetterAlphabet]
+message :: [FourLetterAlphabet]
+message = [L1, L3, L4, L1, L1, L2]
 
--- E.g. fourLetterEncoder message == [L3,L1,L2,L3,L3,L4]
---      fourLetterEncoder (fourLetterEncoder message) == message --> True
+{-
+fourLetterEncoder message -- [L3,L1,L2,L3,L3,L4]
+fourLetterEncoder (fourLetterEncoder message) == message -- True
+-}
 fourLetterEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
 fourLetterEncoder = map rot4l
-  where
-    alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
-    rot4l = rotN alphaSize
+ where
+  alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+  rot4l     = rotN alphaSize
 
 --
---
 -- XOR: The magic of cryptography!
---
 --
 
 xor :: [Bool] -> [Bool] -> [Bool]
 xor bs1 bs2 = map xorPair (zip bs1 bs2)
-    where xorPair (b1, b2) = xorBool b1 b2
-          xorBool b1 b2 = (b1 || b2) && not (b1 && b2)
+ where
+  xorPair (b1, b2) = xorBool b1 b2
+  xorBool b1 b2 = (b1 || b2) && not (b1 && b2)
+
+-- skip --

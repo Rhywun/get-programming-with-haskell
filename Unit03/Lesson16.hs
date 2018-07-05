27725b5e1596610d49ea37162650e82fce3d3881
Patrick Huffer <rhywun@rhywun.com>
2018-07-04 23:27:58 -0400

    End of Lesson16

--- Unit03/Lesson16.hs
+++ Unit03/Lesson16.hs
@@ -1,16 +1,38 @@
 module Lesson16 where
 
---
+-- Consider this
+
+data BreakfastSide = Toast | Biscuit | Homefries | Fruit deriving Show
+data BreakfastMeat = Sausage | Bacon | Ham deriving Show
+data BreakfastMain = Egg | Pancake | Waffle deriving Show
+
+{-
+data BreakfastSpecial = ?
+      Kids’ breakfast — One main and one side
+      or
+      Basic breakfast — One main, one meat, and one side
+      or
+      The lumberjack! — Two mains, two meats, and three sides
+-}
+
 --
 -- Product types - combining types with “and”
 --
---
-data AuthorName = AuthorName
-  { firstName :: String
-  , lastName  :: String
-  }
 
-data BookV1 = BookV1
+type FirstName = String
+type LastName = String
+type ISBN = String
+type Title = String
+type Year = Int
+type Price = Double
+
+newtype AuthorName' = FirstName LastName
+
+data Book'' = AuthorName' ISBN Title Year Price
+
+-- Or, using record sytax:
+
+data Book' = Book'
   { authorV1 :: AuthorName
   , isbnV1   :: String
   , titleV1  :: String
@@ -18,52 +40,51 @@ data BookV1 = BookV1
   , priceV1  :: Double
   }
 
---
+-- QC1
+
+data AuthorName = AuthorName
+  { firstName :: String
+  , lastName  :: String
+  }
+
+-- QC2
+
+data Car = Car
+
+type Spoiler = String
+
+data SportsCar = SportsCar Car Spoiler
+
 --
 -- Sum types - combining types with “or”
 --
---
-type FirstName = String
-
-type LastName = String
 
 type MiddleName = String
 
 data Name
-  = Name FirstName
-         LastName
-  | NameWithMiddle FirstName
-                   MiddleName
-                   LastName
-  | TwoInitialsWithLast Char
-                        Char
-                        LastName
+  = Name FirstName LastName
+  | NameWithMiddle FirstName MiddleName LastName
+  | TwoInitialsWithLast Char Char LastName
   deriving (Show)
 
 --
---
+
 data Creator
   = AuthorCreator Author
   | ArtistCreator Artist
   deriving (Show)
 
-newtype Author =
-  Author Name
-  deriving (Show)
+newtype Author =  Author Name  deriving (Show)
 
-data Artist
-  = Person Name
-  | Band String
-  deriving (Show)
+data Artist  = Person Name  | Band String  deriving (Show)
 
-hpLovecraft =
-  AuthorCreator (Author (TwoInitialsWithLast 'H' 'P' "Lovecraft")) :: Creator
+hpLovecraft :: Creator
+hpLovecraft = AuthorCreator (Author (TwoInitialsWithLast 'H' 'P' "Lovecraft"))
 
---
 --
 -- Putting together your bookstore
 --
---
+
 data Book = Book
   { author    :: Creator
   , isbn      :: String
@@ -92,25 +113,29 @@ data StoreItem
   | PamphletItem Pamphlet
 
 price :: StoreItem -> Double
-price (BookItem book)     = bookPrice book
-price (RecordItem record) = recordPrice record
-price (ToyItem toy)       = toyPrice toy
-price (PamphletItem _)    = 0.0
+price (BookItem     book  ) = bookPrice book
+price (RecordItem   record) = recordPrice record
+price (ToyItem      toy   ) = toyPrice toy
+price (PamphletItem _     ) = 0.0
 
--- QC1603
+-- QC3
+
 madeBy :: StoreItem -> String
-madeBy (BookItem book)     = show $ author book
-madeBy (RecordItem record) = show $ artist record
-madeBy (ToyItem _)         = "Unknown Maker"
+madeBy (BookItem     book  ) = show $ author book
+madeBy (RecordItem   record) = show $ artist record
+madeBy (ToyItem      _     ) = "Unknown Maker"
+madeBy (PamphletItem _     ) = undefined
 
--- Q1601
+-- Q1
+
 data Pamphlet = Pamphlet
   { pamphletTitle      :: String
   , pamphletDescrption :: String
   , pamphletContact    :: String
   }
 
--- Q1602
+-- Q2
+
 type Radius = Double
 
 type Height = Double
@@ -120,15 +145,14 @@ type Width = Double
 data Shape
   = Circle Radius
   | Square Width
-  | Rectangle Height
-              Width
+  | Rectangle Height Width
 
 perimeter :: Shape -> Double
-perimeter (Circle radius)          = 2 * pi * radius
-perimeter (Square width)           = 4 * width
+perimeter (Circle radius         ) = 2 * pi * radius
+perimeter (Square width          ) = 4 * width
 perimeter (Rectangle height width) = 2 * height + 2 * width
 
 area :: Shape -> Double
-area (Circle radius)          = pi * radius ^ 2
-area (Square width)           = width ^ 2
+area (Circle radius         ) = pi * radius ^ (2 :: Int)
+area (Square width          ) = width ^ (2 :: Int)
 area (Rectangle height width) = height * width

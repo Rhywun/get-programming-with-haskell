1d238aef44177159dcb2420ada8cc7e41cbf4d03
Patrick Huffer <rhywun@rhywun.com>
2018-07-14 10:14:31 -0400

    Middle of Lesson29

--- Unit05/Lesson29.hs
+++ Unit05/Lesson29.hs
@@ -8,52 +8,60 @@ module Lesson29 where
     <*>    :: f (a -> b) -> f a -> f b
     pure   :: a -> f a
 -}
---
--- QC2901
+
+-- QC1
+
+qc1 :: Maybe String
 qc1 = (++) <$> Just "hello, " <*> Just "world!" -- == Just "hello, world!"
 
--- QC2902
-qc2 = pure "Hello World!" :: IO String
+-- QC2
 
--- QC2903
+qc2 :: IO String
+qc2 = pure "Hello World!"
+
+-- QC3
+
 -- (pure +) <*> (1,2) <*> (3,4)
 -- It doesn't work because (,) is not an instance of Applicative.
---
+
 --
 -- List as a context
 --
---
+
 -- ex1 == [1500,21000,2500,22000,3500,23000]   i.e. all possible sums
 ex1 = pure (+) <*> [1000, 2000, 3000] <*> [500, 20000]
 
 -- A game show example
---
-doorPrize = [1000, 2000, 3000] :: [Int]
 
-boxPrize = [500, 20000] :: [Int]
+doorPrize :: [Int]
+doorPrize = [1000, 2000, 3000]
 
--- Deterministic:
---   Obviously, this won't compile
+boxPrize :: [Int]
+boxPrize = [500, 20000]
+
+-- Deterministic - obviously, this won't compile:
 -- totalPrize = (+) doorPrize boxPrize
+
 -- Non-deterministic
 totalPrize = pure (+) <*> doorPrize <*> boxPrize
 
---
--- QC2904
+-- QC4
+
 -- qc4 == [10000,50000,20000,100000,30000,150000]
 qc4 = pure (*) <*> doorPrize <*> [10, 50]
 
 --
 -- Prime numbers
 --
+
 -- Simple, if inefficient, prime number generator
 -- E.g. primesToN 32 == [2,3,5,7,11,13,17,19,23,29,31]
 primesToN :: Integer -> [Integer]
 primesToN n = filter notComposite twoToN
-  where
-    twoToN = [2 .. n]
-    composite = pure (*) <*> twoToN <*> twoToN
-    notComposite = not . (`elem` composite)
+ where
+  twoToN       = [2 .. n]
+  composite    = pure (*) <*> twoToN <*> twoToN
+  notComposite = not . (`elem` composite)
 
 --
 -- Generate large amounts of test data
@@ -65,11 +73,7 @@ data User = User
   } deriving (Show)
 
 testNames =
-  [ "John Smith"
-  , "Robert'); DROP TABLE Students;--"
-  , "Christina NULL"
-  , "Randall Munroe"
-  ]
+  ["John Smith", "Robert'); DROP TABLE Students;--", "Christina NULL", "Randall Munroe"]
 
 testIDs = [1337, 0123, 999999]
 
@@ -78,29 +82,29 @@ testScores = [0, 100000, -99999]
 testData :: [User]
 testData = pure User <*> testNames <*> testIDs <*> testScores
 
---
--- QC2905
+-- QC5
+
 testNames' = "Rhywun" : testNames
 
 testData' = pure User <*> testNames' <*> testIDs <*> testScores
 
 qc5 = length testData' -- == 45
 
---
--- Q2901
+-- Q1
+
 allFmap :: Applicative f => (a -> b) -> f a -> f b
 allFmap f x = pure f <*> x
 
---
--- Q2902
+-- Q2
+
 example :: Int
 example = (*) ((+) 2 4) 5 -- == 30
 
 exampleMaybe :: Maybe Int
 exampleMaybe = pure (*) <*> pure ((+) 2 4) <*> pure 5 -- == Just 30
 
---
--- Q2903
+-- Q3
+
 bought = [6, 12]
 
 drank = [-4]
@@ -110,18 +114,14 @@ peeps = [3, 5]
 perPeep = [3, 4]
 
 -- subtract (peeps * perPeep) from bought + drank, answer is the max num of beers
-q3 =
-  pure (-) <*> (pure (+) <*> bought <*> drank) <*>
-  (pure (*) <*> peeps <*> perPeep)
+q3 = pure (-) <*> (pure (+) <*> bought <*> drank) <*> (pure (*) <*> peeps <*> perPeep)
 
 -- q3 == [-7,-10,-13,-18,-1,-4,-7,-12]
 --                   ^^^
 -- Therefore, you'll need to buy 18 beers.
 
---
---
 -- Solution:
---
+
 startingBeer :: [Int]
 startingBeer = [6, 12]
 

900417644476d84d5a32065f627e4cfef7e17efd
Patrick Huffer <rhywun@rhywun.com>
2018-07-21 19:09:24 -0400

    End of Lesson39

--- Unit07/Lesson39/http-lesson/app/Main.hs
+++ Unit07/Lesson39/http-lesson/app/Main.hs
@@ -1,11 +1,32 @@
 module Main where
 
-import qualified Data.ByteString            as B
-import qualified Data.ByteString.Char8      as BC
-import qualified Data.ByteString.Lazy       as L
-import qualified Data.ByteString.Lazy.Char8 as LC
+import qualified Data.ByteString               as B
+import qualified Data.ByteString.Char8         as BC
+import qualified Data.ByteString.Lazy          as L
+import qualified Data.ByteString.Lazy.Char8    as LC
+import           Data.String
 import           Network.HTTP.Simple
+import           Network.HTTP.Types.Header
 import           Network.HTTP.Types.Status
+import           Control.Monad.IO.Class         ( MonadIO )
+
+-- QC2
+
+response :: MonadIO m => m (Response LC.ByteString)
+response = httpLBS "http://news.ycombinator.com"
+
+qc2 :: MonadIO f => f [(HeaderName, BC.ByteString)]
+qc2 = getResponseHeaders <$> response
+
+mainQC2 :: IO ()
+mainQC2 = do
+  response <- httpLBS "http://news.ycombinator.com"
+  print $ getResponseHeader "Server" response
+  -- prints ["nginx"]
+
+--
+-- Making a (more complicated) HTTP request
+--
 
 myToken = "WkWRfDFnAuVytwSTBPTohnvHkcfXuAHx" :: BC.ByteString
 
@@ -13,26 +34,40 @@ noaaHost = "www.ncdc.noaa.gov" :: BC.ByteString
 
 apiPath = "/cdo-web/api/v2/datasets" :: BC.ByteString
 
-buildRequest ::
-     BC.ByteString -> BC.ByteString -> BC.ByteString -> BC.ByteString -> Request
+buildRequest :: BC.ByteString
+             -> BC.ByteString
+             -> BC.ByteString
+             -> BC.ByteString
+             -> Request
 buildRequest token host method path =
-  setRequestMethod method $
-  setRequestHost host $
-  setRequestHeader "token" [token] $
-  setRequestPath path $
-  setRequestSecure True $ setRequestPort 443 defaultRequest
+  setRequestMethod method
+    $ setRequestHost host
+    $ setRequestHeader "token" [token]
+    $ setRequestPath path
+    $ setRequestSecure True
+    $ setRequestPort 443 defaultRequest
 
-buildRequestNoSSL ::
-     BC.ByteString -> BC.ByteString -> BC.ByteString -> BC.ByteString -> Request
+buildRequestNoSSL :: BC.ByteString
+                  -> BC.ByteString
+                  -> BC.ByteString
+                  -> BC.ByteString
+                  -> Request
 buildRequestNoSSL token host method path =
-  setRequestMethod method $
-  setRequestHost host $
-  setRequestHeader "token" [token] $
-  setRequestPath path $
-  setRequestSecure False $ setRequestPort 80 defaultRequest
+  setRequestMethod method
+    $ setRequestHost host
+    $ setRequestHeader "token" [token]
+    $ setRequestPath path
+    $ setRequestSecure False
+    $ setRequestPort 80 defaultRequest
 
--- request = buildRequest myToken noaaHost "GET" apiPath :: Request
-request = buildRequestNoSSL myToken noaaHost "GET" apiPath :: Request
+request = buildRequest myToken noaaHost "GET" apiPath :: Request
+
+-- Q1
+request' = buildRequestNoSSL myToken noaaHost "GET" apiPath :: Request
+
+--
+-- Putting it all together
+--
 
 main :: IO ()
 main = do

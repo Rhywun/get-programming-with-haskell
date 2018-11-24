318dedaf1cb51fba7d442a03df2c415c541b95b6
Patrick Huffer <rhywun@rhywun.com>
2018-11-24 03:24:41 -0500

    Revisited Lesson35

--- Unit06/Lesson35/palindrome-checker/app/Main.hs
+++ Unit06/Lesson35/palindrome-checker/app/Main.hs
@@ -3,10 +3,12 @@ module Main where
 import           Lib
 import           Data.Text                     as T
 import           Data.Text.IO                  as TIO
+import           System.IO
 
 main :: IO ()
 main = do
-  TIO.putStrLn "Text?"
+  hSetBuffering stdout NoBuffering
+  TIO.putStr "Text? "
   text <- TIO.getLine
   let response = if isPalindrome text then "Palindrome!" else "No palindrome :("
   TIO.putStrLn response

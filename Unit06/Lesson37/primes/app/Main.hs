1549ebc2a5896c891dfcaef4c615c77cc808bebf
Patrick Huffer <rhywun@rhywun.com>
2018-12-27 20:13:00 -0500

    Revisited Lesson37

--- Unit06/Lesson37/primes/app/Main.hs
+++ Unit06/Lesson37/primes/app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import Primes
+import           Primes
+import           System.IO
 
 main :: IO ()
-main = return ()
+main = do
+  hSetBuffering stdout NoBuffering
+  putStr "Enter a number to check if it's prime: "
+  num <- getLine
+  let result = isPrime (read num :: Int) -- TODO: Need error-checking here
+                                         -- This is the subject of the next lesson
+  case result of
+    Just True  -> putStrLn "It is prime!"
+    Just False -> putStrLn "It's not prime."
+    Nothing    -> putStrLn "Sorry, this number is not a valid candidate for testing."
+
+-- re: Miller-Rabin primality test
+-- --reads--
+-- Hard pass...
